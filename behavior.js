a0cd41affa6d4613ca729b11558d5e7d770e55fc
Alex Sherzai <sherzaiscience@gmail.com>
2021-07-25 16:02:06 -0700

    Upload 3

--- behavior.js
+++ behavior.js
@@ -1,7 +1,7 @@
-var att_scores = [40, 50, 60, 70, 80, 90, 100, 0];
-var visuo_scores = [11, 6, 3, 9, 10, 8, 0];
-var memory_scores = [20, 16, 16, 14, 12, 21, 0];
-var speed_scores = [51, 63, 42, 41, 70, 89, 0];
+var att_scores = [40, 50, 60, 70, 80, 90, 100];
+var visuo_scores = [11, 6, 3, 9, 10, 8];
+var memory_scores = [20, 16, 16, 14, 12, 21];
+var speed_scores = [51, 63, 42, 41, 70, 89];
 
 sessionStorage.setItem("logged-in", false);
 
