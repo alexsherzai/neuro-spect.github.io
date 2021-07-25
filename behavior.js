0ff48ed9fd5b701aae964872675c48150f861bcb
Alex Sherzai <sherzaiscience@gmail.com>
2021-07-25 15:23:28 -0700

    Upload 1

---
+++ behavior.js
@@ -0,0 +1,18 @@
+var att_scores = [40, 50, 60, 70, 80, 90, 100, 0];
+var visuo_scores = [11, 6, 3, 9, 10, 8, 0];
+var memory_scores = [20, 16, 16, 14, 12, 21, 0];
+var speed_scores = [51, 63, 42, 41, 70, 89, 0];
+
+sessionStorage.setItem("logged-in", false);
+
+document.getElementById('attention').setAttribute("data-percent", att_scores[att_scores.length - 1].toString());
+document.getElementById('attention').innerHTML = att_scores[att_scores.length - 1].toString();
+
+document.getElementById('visuospatial').setAttribute("data-percent", visuo_scores[visuo_scores.length - 1].toString());
+document.getElementById('visuospatial').innerHTML = visuo_scores[visuo_scores.length - 1].toString();
+
+document.getElementById('memory').setAttribute("data-percent", memory_scores[memory_scores.length - 1].toString());
+document.getElementById('memory').innerHTML = memory_scores[memory_scores.length - 1].toString();
+
+document.getElementById('speed').setAttribute("data-percent", speed_scores[speed_scores.length - 1].toString());
+document.getElementById('speed').innerHTML = speed_scores[speed_scores.length - 1].toString();
