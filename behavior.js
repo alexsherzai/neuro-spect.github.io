2e7b8e36984fd2e6f606573f3e259a4f4fd5557d
Alex Sherzai <sherzaiscience@gmail.com>
2021-08-13 15:06:30 -0700

    e

--- behavior.js
+++ behavior.js
@@ -3,7 +3,34 @@ var visuo_scores = [11, 6, 3, 9, 10, 8];
 var memory_scores = [20, 16, 16, 14, 12, 21];
 var speed_scores = [51, 63, 42, 41, 70, 89];
 
-sessionStorage.setItem("logged-in", false);
+sessionStorage.setItem("logged-in", "true");
+
+function onSubmit() {
+    sessionStorage.setItem("logged-in", "true");
+    window.location.href = "index.html";
+};
+
+
+
+if (sessionStorage.getItem("logged-in") == "false") {
+    document.getElementById('Resources').style.display = "block";
+    document.getElementById('progress-title').style.display = "none";
+    document.getElementById('progress').style.display = "none";
+    document.getElementById('trend-title').style.display = "none";
+    document.getElementById('trend').style.display = "none";
+} else if (sessionStorage.getItem("logged-in") == "true") {
+    var parts = document.getElementById("e-mail").value.split('@');
+
+    document.getElementById('reg-button').style.display = "none";
+    document.getElementById('login-button').style.display = "none";
+    document.getElementById('Welcome').innerHTML = "Welcome " + parts[0] + "!"
+
+    document.getElementById('Resources').style.display = "none";
+    document.getElementById('progress-title').style.display = "block";
+    document.getElementById('progress').style.display = "block";
+    document.getElementById('trend-title').style.display = "block";
+    document.getElementById('trend').style.display = "block";
+}
 
 document.getElementById('attention').setAttribute("data-percent", att_scores[att_scores.length - 1].toString());
 document.getElementById('attention').innerHTML = att_scores[att_scores.length - 1].toString();
